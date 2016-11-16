f9638e23c771a8193f83aba318e28c8ca326831e
antoni29 <vallegalvez.93@gmail.com>
2016-11-16 12:01:18 -0700

    consultas con ajax

--- js/eventos.js
+++ js/eventos.js
@@ -3,7 +3,16 @@ var inicio = function()
 {
 	var dameclic = function()
 	{
-		alert("Le di clic a un botón >0<");
+		$.ajax({
+		url: 'https://randomuser.me/api/',
+		dataType: 'json',
+		success: function(data) {
+		$("#txtNombre").val(data.results[0].name.first+" "+data.results[0].name.last);
+		$("#imgFoto").attr("src",data.results[0].picture.large);
+		console.log(data.results[0].name.first+" "+data.results[0].name.last);
+  		}
+	});
+      
 	}
 	$("#dameClic").on("click",dameclic);
 }
